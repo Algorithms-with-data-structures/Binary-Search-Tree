646ba522295b95640142a9aacc5ffa8e5700b327
Patrik <praprotnik.patrik@gmail.com>
2024-12-27 18:08:47 +0100

    Refactoring

--- Vaja4/Vaja4.cpp
+++ Vaja4/Vaja4.cpp
@@ -1,308 +1,290 @@
-//BINARNO ISKLANO DREVO
+// BINARY SEARCH TREE
 
 #include <iostream>
 using namespace std;
 
 struct Node {
-	int key = 0;
-	Node* father = nullptr;
-	Node* leftSon = nullptr;
-	Node* rightSon = nullptr;
+    int key = 0;
+    Node* parent = nullptr;
+    Node* left = nullptr;
+    Node* right = nullptr;
 };
 
-void menu() {
-	cout << endl;
-	cout << "Binarno iskalno drevo - izbira: " << endl;
-	cout << endl;
-	cout << "1) Vnos podatka" << endl;
-	cout << "2) Urejen izpis vrednosti" << endl;
-	cout << "3) Izpis povezav" << endl;
-	cout << "4) Iskanje" << endl;
-	cout << "5) Poisci minimum in maksimum" << endl;
-	cout << "6) Poisci predhodnika in naslednika" << endl;
-	cout << "7) Brisi vrednost" << endl;
-	cout << "8) Konec" << endl;
-	cout << endl;
-	cout << "Izbira: ";
+
+// Helper function to display the menu
+void displayMenu() {
+    cout << "\nBinary Search Tree - Options:\n";
+    cout << "1) Insert a value\n";
+    cout << "2) Print sorted values\n";
+    cout << "3) Print connections\n";
+    cout << "4) Search for a value\n";
+    cout << "5) Find minimum and maximum\n";
+    cout << "6) Find predecessor and successor\n";
+    cout << "7) Delete a value\n";
+    cout << "8) Exit\n";
+    cout << "\nChoose: ";
 }
 
+// Insert a value into the BST
+void insert(int value, Node*& root) {
+    Node* parent = nullptr;
+    Node* current = root;
 
-void insert(int stevilo, Node*& koren) {
-	Node* y = nullptr;
-	Node* temp = koren;
+    // Find the parent node for the new value
+    while (current != nullptr) {
+        parent = current;
+        if (value < current->key) {
+            current = current->left;
+        }
+        else if (value > current->key) {
+            current = current->right;
+        }
+        else {
+            cout << "ERROR: Duplicate value not allowed.\n";
+            return;
+        }
+    }
 
-	while (temp != nullptr) {				//poišèemo "primeren" kazalec oz. mesto za nov element
-		y = temp;
-		if (stevilo < temp->key) {
-			temp = temp->leftSon;
-		}
-		else if (stevilo > temp->key) {
-			temp = temp->rightSon;
-		}
-		else
-			cout << "ERROR";
-	}
+    // Create a new node
+    Node* newNode = new Node{ value, parent, nullptr, nullptr };
 
-	Node* new_element = new Node;
-	new_element->key = stevilo;				//ustvarimo nov element s kazalcem father na prej najdeno mesto
-	new_element->father = y;
-
-	if (y == nullptr) {
-		koren = new_element;				//èe nismo našli primernega mesta za nov objekt, pomeni da drevo še ne obstaja
-	}
-	else {
-		if (new_element->key < y->key) {	//èe je nov element manjši od "najdenega mesta", postane leftSon
-			y->leftSon = new_element;		
-		}
-		else {
-			y->rightSon = new_element;		//... drugaèe rightSon
-		}
-	
-	}
+    if (parent == nullptr) {
+        root = newNode; // The tree was empty, new node becomes the root
+    }
+    else if (value < parent->key) {
+        parent->left = newNode;
+    }
+    else {
+        parent->right = newNode;
+    }
 }
 
-
-Node* search(int stevilo, Node*& koren) {
-	Node* temp = koren;
-
-	while (temp != nullptr) {
-		if (stevilo == temp->key) {
-			return temp;
-		}
-		if (stevilo < temp->key) {
-			temp = temp->leftSon;
-		}
-		else {
-			temp = temp->rightSon;
-		}
-	}
-
-	return temp;
+// Search for a value in the BST
+Node* search(int value, Node* root) {
+    Node* current = root;
+    while (current != nullptr && current->key != value) {
+        current = (value < current->key) ? current->left : current->right;
+    }
+    return current;
 }
 
-
-void urejenIzpis(Node*& x) {
-	if (x != nullptr) {
-		urejenIzpis(x->leftSon);
-		cout << x->key << " ";
-		urejenIzpis(x->rightSon);
-	}
+// Print the tree in sorted order (in-order traversal)
+void printSorted(Node* node) {
+    if (node != nullptr) {
+        printSorted(node->left);
+        cout << node->key << " ";
+        printSorted(node->right);
+    }
 }
 
+// Print the connections of the BST nodes
+void printConnections(Node* node) {
+    if (node == nullptr) return;
 
-void izpisPovezav(Node*& x) {
-	if (x->leftSon != nullptr) {
-		cout << x->key << "->" << x->leftSon->key << endl;
-		izpisPovezav(x->leftSon);
-	}
+    if (node->left != nullptr) {
+        cout << node->key << " -> " << node->left->key << endl;
+        printConnections(node->left);
+    }
 
-	if (x->rightSon != nullptr) {
-		cout << x->key << "->" << x->rightSon->key << endl;
-		izpisPovezav(x->rightSon);
-	}
+    if (node->right != nullptr) {
+        cout << node->key << " -> " << node->right->key << endl;
+        printConnections(node->right);
+    }
 }
 
-
-Node* minimum(Node*& koren) {
-	Node* temp = koren;
-	while (temp->leftSon != nullptr) {
-		temp = temp->leftSon;
-	}
-
-	return temp;
+// Find the minimum value in the BST
+Node* findMinimum(Node* root) {
+    Node* current = root;
+    while (current != nullptr && current->left != nullptr) {
+        current = current->left;
+    }
+    return current;
 }
 
-Node* maximum(Node*& koren) {
-	Node* temp = koren;
-	while (temp->rightSon != nullptr) {
-		temp = temp->rightSon;
-	}
-
-	return temp;
+// Find the maximum value in the BST
+Node* findMaximum(Node* root) {
+    Node* current = root;
+    while (current != nullptr && current->right != nullptr) {
+        current = current->right;
+    }
+    return current;
 }
 
+// Find the predecessor of a given node
+Node* findPredecessor(Node* node) {
+    if (node->left != nullptr) {
+        return findMaximum(node->left);
+    }
 
-Node* predhodnik(Node* temp) {
-	if (temp->leftSon != nullptr) {
-		return maximum(temp->leftSon);
-	}
+    Node* parent = node->parent;
+    while (parent != nullptr && node == parent->left) {
+        node = parent;
+        parent = parent->parent;
+    }
 
-	Node* y = temp->father;
-	while (y != nullptr && temp == y->leftSon) {
-		temp = y;
-		y = y->father;
-	}
-
-	return y;
+    return parent;
 }
 
-Node* naslednik(Node* temp) {
-	if (temp->rightSon != nullptr) {
-		return minimum(temp->rightSon);
-	}
+// Find the successor of a given node
+Node* findSuccessor(Node* node) {
+    if (node->right != nullptr) {
+        return findMinimum(node->right);
+    }
 
-	Node* y = temp->father;
-	while (y != nullptr && temp == y->rightSon) {
-		temp = y;
-		y = y->father;
-	}
+    Node* parent = node->parent;
+    while (parent != nullptr && node == parent->right) {
+        node = parent;
+        parent = parent->parent;
+    }
 
-	return y;
+    return parent;
 }
 
-void brisiScenarij1_2(Node*& temp, Node*& koren) {
-	Node* x;
-	if (temp->leftSon != nullptr) {
-		x = temp->leftSon;
-	}									//kazalec x kaže na levi ali desni sin elementa
-	else {
-		x = temp->rightSon;		
-	}
+// Handle deletion for nodes with 0 or 1 child
+void deleteCase1Or2(Node*& target, Node*& root) {
+    Node* child = (target->left != nullptr) ? target->left : target->right;
 
-	if (x != nullptr) {
-		x->father = temp->father;		//oèe brisanega števila postane oèe sina brisanega števila 
-	}
+    if (child != nullptr) {
+        child->parent = target->parent;
+    }
 
-	if (temp->father == nullptr) {		//èe brišemo prvi element
-		koren = x;
-	}
-	else {
-		if (temp == temp->father->leftSon) {
-			temp->father->leftSon = x;		//prevežemo da starševski element kaže na kazalec x
-		}
-		else {
-			temp->father->rightSon = x;
-		}
-	}
+    if (target->parent == nullptr) {
+        root = child;
+    }
+    else if (target == target->parent->left) {
+        target->parent->left = child;
+    }
+    else {
+        target->parent->right = child;
+    }
+
+    delete target;
 }
 
-void brisi(Node*& temp, Node*& koren) {
-	Node* y = new Node;
-
-	if (temp->leftSon == nullptr || temp->rightSon == nullptr) {
-		brisiScenarij1_2(temp, koren);
-	}
-	else {
-		y = naslednik(temp);
-		int vrednost = y->key;
-		brisiScenarij1_2(y, koren);
-		temp->key = vrednost;
-	}
+// Delete a node from the BST
+void deleteNode(Node*& target, Node*& root) {
+    if (target->left == nullptr || target->right == nullptr) {
+        deleteCase1Or2(target, root);
+    }
+    else {
+        Node* successor = findSuccessor(target);
+        target->key = successor->key;
+        deleteCase1Or2(successor, root);
+    }
 }
 
-int main(){
-	bool running = true;
-	int izbira;
+int main() {
+    Node* root = nullptr;
+    bool running = true;
 
-	Node* koren = nullptr;
-	int stevilo;
+    while (running) {
+        displayMenu();
+        int choice, value;
+        cin >> choice;
+        system("cls");
 
-	Node* Minimum;
-	Node* Maximum;
-	Node* Predhodnik;
-	Node* Naslednik;
-	Node* pointerToSt;
+        switch (choice) {
+        case 1:
+            cout << "Enter value to insert: ";
+            cin >> value;
+            insert(value, root);
+            break;
 
-	do {
-		cout << endl;
-		menu();
-		cin >> izbira;
-		switch (izbira) {
-		case 1:
-			cout << "Katero stevilo zelite vnesti: ";
-			cin >> stevilo;
-			while (search(stevilo, koren) != nullptr) {
-				cout << "Stevilo ze obstaja. Vnes novo: ";
-				cin >> stevilo;
-			}
-			insert(stevilo, koren);
-			break;
-		case 2:
-			if (koren == nullptr) {
-				cout << "Drevo je prazno!" << endl;
-			}
-			else {
-				urejenIzpis(koren);
-			}
-			break;
-		case 3:
-			if (koren == nullptr) {
-				cout << "Drevo je prazno!" << endl;
-			}
-			else {
-				izpisPovezav(koren);
-			}
-			break;
-		case 4:
-			cout << "Katero stevilo iscete: ";
-			cin >> stevilo;
-			if (search(stevilo, koren) != nullptr) {		//èe vrne kazalec na to število, potem število obstaja, drugaèe ne (vrne nullptr)
-				cout << "Stevilo obstaja v drevesu." << endl;
-			}
-			else {
-				cout << "Stevilo v drevesu ne obstaja." << endl;
-			}
-			break;
-		case 5:			
-			if (koren == nullptr) {
-				cout << "Drevo je prazno!";
-			}
-			else {
-				Minimum = minimum(koren);
-				Maximum = maximum(koren);
-				cout << "Najmanjsa vrednost v drevesu: " << Minimum->key /*minimum(koren)*/ << endl;
-				cout << "Najvecja vrednost v drevesu: " << Maximum->key /*maximum(koren)*/;
-			}
-			break;
-		case 6:
-			cout << "Predhodnik & naslednik katerega stevila: ";
-			cin >> stevilo;
-			if(search(stevilo, koren) == nullptr) {
-				cout << "Stevilo ne obstaja.";
-				break;
-			}
-			else {
-				Predhodnik = predhodnik(search(stevilo, koren));
-				Naslednik = naslednik(search(stevilo, koren));
-				if (Predhodnik == nullptr) {
-					cout << "Predhodnik ne obstaja!" << endl;
-				}
-				else {
-					cout << "Predhodnik: " << Predhodnik->key /*predhodnik(search(stevilo))*/ << endl;
-				}
+        case 2:
+            if (root == nullptr) {
+                cout << "Tree is empty.\n";
+            }
+            else {
+                printSorted(root);
+                cout << endl;
+            }
+            break;
 
-				if (Naslednik == nullptr) {
-					cout << "Naslednik ne obstaja!" << endl;
-				}
-				else {
-					cout << "Naslednik: " << Naslednik->key /*naslednik(search(stevilo))*/ << endl;
-				}
-			}
+        case 3:
+            if (root == nullptr) {
+                cout << "Tree is empty.\n";
+            }
+            else {
+                printConnections(root);
+            }
+            break;
 
+        case 4:
+            cout << "Enter value to search: ";
+            cin >> value;
+            if (search(value, root) != nullptr) {
+                cout << "Value found in the tree.\n";
+            }
+            else {
+                cout << "Value not found.\n";
+            }
+            break;
 
-			break;
-		case 7:
-			cout << "Katero stevilo zelite zbrisat: ";
-			cin >> stevilo;
-			if (search(stevilo, koren) == nullptr) {
-				cout << "Stevilo ne obstaja.";
-				break;
-			}
-			else {
-				pointerToSt = search(stevilo, koren);
-				brisi(pointerToSt, koren);
-			}
-			break;
-		case 8:
-			running = false;
-			delete koren;
-			break;
-		default:
-			cout << "Narobe izbira!" << endl;
-			break;
-		}
-	} while (running);
-	
-	return 0;
+        case 5:
+            if (root == nullptr) {
+                cout << "Tree is empty.\n";
+            }
+            else {
+                Node* minNode = findMinimum(root);
+                Node* maxNode = findMaximum(root);
+                cout << "Minimum value: " << minNode->key << endl;
+                cout << "Maximum value: " << maxNode->key << endl;
+            }
+            break;
+
+        case 6:
+            cout << "Enter value to find predecessor and successor: ";
+            cin >> value;
+            Node* target;
+            target = search(value, root);
+
+            if (target == nullptr) {
+                cout << "Value not found.\n";
+            }
+            else {
+                Node* predecessor = findPredecessor(target);
+                Node* successor = findSuccessor(target);
+
+                if (predecessor != nullptr) {
+                    cout << "Predecessor: " << predecessor->key << endl;
+                }
+                else {
+                    cout << "No predecessor exists.\n";
+                }
+
+                if (successor != nullptr) {
+                    cout << "Successor: " << successor->key << endl;
+                }
+                else {
+                    cout << "No successor exists.\n";
+                }
+            }
+            break;
+
+        case 7:
+            cout << "Enter value to delete: ";
+            cin >> value;
+            Node* nodeToDelete;
+            nodeToDelete = search(value, root);
+            if (nodeToDelete == nullptr) {
+                cout << "Value not found.\n";
+            }
+            else {
+                deleteNode(nodeToDelete, root);
+                cout << "Value deleted.\n";
+            }
+            break;
+
+        case 8:
+            running = false;
+            cout << "Exiting...\n";
+            break;
+
+        default:
+            cout << "Invalid choice. Try again.\n";
+            break;
+        }
+    }
+
+    return 0;
 }
-
